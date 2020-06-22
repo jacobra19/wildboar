c393d6a4b1638749eb588aa472fbd04057bdd2c9
Yakov Rakhamimov <jacobra19@gmail.com>
2020-06-23 01:15:12 +0300

    basic usage enabled

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    constructor() {
+        super();
+    }
+
+
+    render() {
+        return (
+            <div>
+                hello app
+            </div>
+        );
+    }
+}
+
+export default App;
