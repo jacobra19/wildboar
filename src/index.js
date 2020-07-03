5fdec0b59a31ca9f37714d3b80b8b87c44cad7c7
Yakov Rakhamimov <jacobra19@gmail.com>
2020-07-03 23:02:02 +0300

    main div id name

--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import App from './app'
 
-const wrapper = document.getElementById("container");
-wrapper ? ReactDOM.render(<App />, wrapper) : false;
+ReactDOM.render(<App />, document.getElementById("root"))
