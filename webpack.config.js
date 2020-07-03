fdc03ab751c093711e6e2eb0298a961527871da6
Yakov Rakhamimov <jacobra19@gmail.com>
2020-07-03 23:06:25 +0300

    webpack config css loader and file loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,45 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        },
-        {
-          test: /\.html$/,
-          use: [
+        rules: [
             {
-              loader: "html-loader"
-            }
-          ]
-        }
-      ]
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ]
     },
     plugins: [
-      new HtmlWebPackPlugin({
-        template: "./src/index.html",
-        filename: "./index.html",
-        favicon: "./src/assets/icon.png",
-      })
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html",
+            favicon: "./src/assets/icon.png",
+        })
     ],
     devServer: {
         port: 2020
     }
-  };
+};
