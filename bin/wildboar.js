6c59f7a0529655caef8a59fc45f20d7fab95d805
Yakov Rakhamimov <jacobra19@gmail.com>
2020-07-07 17:07:58 +0300

    bin file

--- bin/wildboar.js
+++ bin/wildboar.js
@@ -1,15 +1,41 @@
 #! /usr/bin/env node
-
 'use strict';
 
-const appName = process.argv[2];
-const { spawnSync } = require('child_process');
-const url = 'https://github.com/jacobra19/wildboar.git';
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
 
-spawnSync('git', ['clone', url, `${process.cwd()}/${appName}`]);
-// spawnSync('npm', ['install', '--prefix', `${process.cwd()}/${appName}`]);
+clear();
 
-console.log('Your app has been successfully installed');
-console.log(`$ cd ${appName}`);
-console.log(`$ npm install`);
-console.log('$ npm start');
+try {
+    const appName = process.argv[2] || 'wildboar-app';
+    const { spawnSync } = require('child_process');
+    const url = 'https://github.com/jacobra19/wildboar.git';
+
+
+    spawnSync('git', ['clone', url, `${process.cwd()}/${appName}`]);
+    spawnSync('cd', [`${process.cwd()}/${appName}`]);
+    spawnSync('del', [`/F /S /Q /A .git`]);
+    spawnSync('rmdir', [`.git`]);
+
+    console.log(
+        chalk.yellow(
+            figlet.textSync('Wildboar.js', { font: 'ANSI Shadow' })
+        )
+    );
+
+    console.log(chalk.green(`Your app has been successfully installed`))
+
+    console.log(chalk.yellow(`
+        cd ${appName}
+        npm install
+        npm start
+
+    `));
+} catch (error) {
+    console.log(
+        chalk.redBright.bold(`Something went wrong, check error:`)
+    );
+    console.log(error);
+
+}
